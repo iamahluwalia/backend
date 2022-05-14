68ae64e7c9bb25068a3e92ae424b2fa685127af8
unknown <rahulaw36@gmail.com>
2022-05-15 02:56:08 +0530

    dupatta

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ require('dotenv').config();
 app.use(cors());
 
 //Using Body Parser
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 //importing models
 require('./Models/mainSchema')
